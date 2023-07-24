d18d5630284fe3341d2f23deef9bdc175398220b
Nikita Skobelkin <nikitaskobelkin@mail.ru>
2023-07-24 23:29:30 +0100

    Initial Commit

---
+++ BragiDemo/BragiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BragiDemoApp.swift
+//  BragiDemo
+//
+//  Created by Nikita Skobelkin on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BragiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
