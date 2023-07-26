97fd2acd65a93ac295c53faacb472f19e62e7d93
Nikita Skobelkin <nikita@apple.com>
2023-07-26 21:58:13 +0100

    Remove extra task wrapper

--- BragiDemo/Shared/Extensions/ScrollView+.swift
+++ BragiDemo/Shared/Extensions/ScrollView+.swift
@@ -12,9 +12,7 @@ extension ScrollView {
     @ViewBuilder func onRefresh(_ action: @escaping () async -> Void) -> some View {
         if #available(iOS 15.0, *) {
             self.refreshable {
-                Task { @MainActor in
-                    await action()
-                }
+                await action()
             }
         } else {
             // MARK: Instead of instrospect we should create specific Wrapper for UIScrollView using UIKIt, in this task I don't have time for it, that's why I'm using Introspect for short simulate case
